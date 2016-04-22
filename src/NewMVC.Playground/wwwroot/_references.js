70dbec30dc31a432821c6eba2715bc7c4a9ece34
Anthony Ma <ama@medkoder.com>
2016-04-22 14:43:22 -0500

    Re-apply gitignore

--- src/NewMVC.Playground/wwwroot/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="../source/js/exporting.js" />
-/// <reference path="../webpack.config.js" />
-/// <reference path="prod/main.bundle.js" />
